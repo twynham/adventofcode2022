b1fabe325d63eca0a0dea1c81995f84f52a11ae7
stewarttwynham <stewart@twynham.com>
2022-12-03 21:23:28 +0000

    December 3rd GREEN unit test pass

--- December03/rucksacks.py
+++ December03/rucksacks.py
@@ -1,5 +1,18 @@
 def process_rucksack(inputdata):
-    return 0
+    total = 0
+    for lines in inputdata.split('\n'):
+        midpoint = int(len(lines)/2)
+        compartmentA = lines[0:midpoint]
+        compartmentB = lines[midpoint:midpoint*2]
+        found = ""
+        for i in range(midpoint):
+            if compartmentB.find(compartmentA[i]) > -1:
+                found = compartmentA[i]
+        priority = ord(found) - 96
+        if priority < 0:
+            priority = ord(found) - 38
+        total += priority
+    return total
 
 
 with open('prodinput.txt') as f:
