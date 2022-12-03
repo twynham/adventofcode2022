63070d20d34b0085349f7647b882376eebd2c1c4
stewarttwynham <stewart@twynham.com>
2022-12-03 21:49:44 +0000

    December 3rd GREEN unit test pass second part

--- December03/rucksacks.py
+++ December03/rucksacks.py
@@ -15,6 +15,37 @@ def process_rucksack(inputdata):
     return total
 
 
+def process_groupings(inputdata):
+    total = 0
+    lineno = 1
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    for lines in inputdata.split('\n'):
+        if (lineno + 2)%3 == 0:
+            line1 = lines
+        if (lineno + 1)%3 == 0:
+            line2 = lines
+        if lineno%3 == 0:
+            line3 = lines
+            found = ""
+            foundin2 = ""
+            for i in range(len(line1)):
+                if line2.find(line1[i]) > -1:
+                    foundin2 = foundin2 + line1[i]
+            for i in range(len(foundin2)):
+                if line3.find(foundin2[i]) > -1:
+                    found = foundin2[i]
+            priority = ord(found) - 96
+            if priority < 0:
+                priority = ord(found) - 38
+            total += priority
+
+        lineno += 1
+    return total
+
+
 with open('prodinput.txt') as f:
     prodData = f.read()
     print(process_rucksack(prodData))
+    print(process_groupings(prodData))
--- December03/test-rucksacks.py
+++ December03/test-rucksacks.py
@@ -10,6 +10,10 @@ class MyTestCase(unittest.TestCase):
     def test_validresult(self):
         self.assertEqual(157, rucksacks.process_rucksack(testData))  # add assertion here
 
+    def test_validgrouping(self):
+        self.assertEqual(70, rucksacks.process_groupings(testData))  # add assertion here
+
+
 
 if __name__ == '__main__':
     unittest.main()
