8cd653afa2e8185b86a4bac942986581b8fe1770
stewarttwynham <stewart@twynham.com>
2022-12-04 20:11:08 +0000

    December 4th RED unit test fail

---
+++ December04/cleanup.py
@@ -0,0 +1,7 @@
+def process(inputdata):
+    return 0
+
+
+with open('prodinput.txt') as f:
+    prodData = f.read()
+    print(process(prodData))
---
+++ December04/test-cleanup.py
@@ -0,0 +1,16 @@
+import unittest
+import cleanup
+
+
+with open('testinput.txt') as f:
+    testData = f.read()
+
+
+class MyTestCase(unittest.TestCase):
+    def test_validresult(self):
+        self.assertEqual(157, cleanup.process(testData))  # add assertion here
+
+
+
+if __name__ == '__main__':
+    unittest.main()
